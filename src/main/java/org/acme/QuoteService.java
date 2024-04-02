2e5681b6627fdd959838ad8bc14503b9459b4a48
musevarg <gravesum5@gmail.com>
2024-04-02 18:41:55 +0200

    created QuoteService

---
+++ src/main/java/org/acme/QuoteService.java
@@ -0,0 +1,19 @@
+package org.acme;
+
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+public class QuoteService {
+    @Inject
+    EntityManager em;
+
+    @Transactional
+    public void createQuote(Long id, String quote, String author) {
+        Quote q = new Quote();
+        q.setId(id);
+        q.setQuote(quote);
+        q.setAuthor(author);
+        em.persist(q);
+    }
+}
