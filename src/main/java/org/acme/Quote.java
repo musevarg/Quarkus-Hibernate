73e1d12128ccd64f5482bb26d83ce9ce6271ad44
musevarg <gravesum5@gmail.com>
2024-04-02 18:39:01 +0200

    getters and setters

--- src/main/java/org/acme/Quote.java
+++ src/main/java/org/acme/Quote.java
@@ -13,4 +13,28 @@ public class Quote {
 
     private String author;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
 }
