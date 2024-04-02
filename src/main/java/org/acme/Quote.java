b476ee2960fca2597cd7450dad1b4fdd5bcb6f92
musevarg <gravesum5@gmail.com>
2024-04-02 18:33:19 +0200

    quote object

--- src/main/java/org/acme/Quote.java
+++ src/main/java/org/acme/Quote.java
@@ -1,5 +1,16 @@
 package org.acme;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Quote {
+
+    @Id
+    private Long id;
+
+    private String quote;
+
+    private String author;
 
 }
